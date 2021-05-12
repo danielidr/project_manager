9836eaf40dcdd4580277423e17b42b6026222b0f
Danielid Ramirez <danielid132@gmail.com>
2021-05-11 22:55:36 -0400

    Modifiying view create and adding validates

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,6 +4,7 @@ class ProjectsController < ApplicationController
   end
 
   def create
+    @project = Project.create(name: params[:name], description: params[:description], start_date: params[:start_date], end_date: params[:end_date], state: params[:state])
   end
 
   def dashboard
--- db/schema.rb
+++ db/schema.rb
@@ -14,12 +14,12 @@ ActiveRecord::Schema.define(version: 2021_05_10_020615) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.text "description"
     t.date "start_date"
     t.date "end_date"
     t.string "state"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
